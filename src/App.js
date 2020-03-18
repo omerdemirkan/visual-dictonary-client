df8f891e8f98c3f15f8476bfb4106ea07ec670c0
Omer Demirkan <omerfarukpiano@gmail.com>
2020-03-18 08:55:23 -0700

    create routes for Home and Search, create pages folder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './App.css';
 
+import { Switch, Route } from 'react-router-dom';
+
 //components
 import Header from './components/Header/Header';
+
+//pages
+import Home from './pages/Home/Home';
+import Search from './pages/Search/Search';
+
 
 function App() {
 
 
   return <div className='App'>
     <Header/>
-    
+
+    <Switch>
+      <Route path='/search' component={Search}/>
+      <Route path='/' component={Home}/>
+    </Switch>
+
   </div>
 }
 
