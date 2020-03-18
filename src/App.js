1f9285688c985d58b8c604f7b7a9458eaa0a6166
Omer Demirkan <omerfarukpiano@gmail.com>
2020-03-17 17:09:40 -0700

    shift file structure to transition to client repo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    
+  }, []);
+
+
   return <div>
     <h1>Yoo</h1>
   </div>
