5fdd657f58ab983351c1fb6c29991eccac3c9db2
hamittokay <hamittokay.me@gmail.com>
2020-03-20 20:54:03 -0700

    Fix: Linting, Design Improvements, Improve Logic

--- src/pages/Search/Result/Result.js
+++ src/pages/Search/Result/Result.js
@@ -9,63 +9,87 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 //Utils
 import applyAccent from '../../../utils/applyAccent';
 
-
 function Result(props) {
+  if (props.loading === true) {
+    return (
+      <div className={classes.SpinnerBox}>
+        <Spinner />
+      </div>
+    );
+  } else if (!props.video) {
+    return null;
+  }
 
-    if (props.loading === true) {
-        return <div className={classes.SpinnerBox}>
-            <Spinner/>
+  function renderPagination() {
+    return (
+      <div className={classes.NodeBox}>
+        {new Array(props.numVideos).fill(null).map((_, idx) => (
+          <span
+            key={idx}
+            className={classes.Node}
+            style={idx === props.videoIndex ? { opacity: '.9' } : null}
+            onClick={() => props.setVideoIndex(idx)}
+            role='button'
+          />
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className={classes.Result}>
+      <div className={classes.VideoBox}>
+        <div
+          style={{
+            boxShadow: '0px 10px 30px #00000050',
+            borderRadius: '10px',
+            overflow: 'hidden',
+            height: '100%'
+          }}
+        >
+          <iframe
+            title='main'
+            width='100%'
+            height='100%'
+            src={`https://www.youtube-nocookie.com/embed/${props.video.id}?controls=0&start=${props.video.start}&end=${props.video.end}&autoplay=true`}
+            frameBorder={0}
+            allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen
+          ></iframe>
         </div>
-    } else if (!props.video) {
-        return null;
-    }
 
-    // Creates mappable array for nodes
-    const nodeList = [];
-    for (let i = 0; i < props.numVideos; i++) {
-        nodeList.push({
-            active: i === props.videoIndex,
-            clicked: () => props.setVideoIndex(i)
-        });
-    }
+        {/* Video Navigation Elements:  */}
+        {props.numVideos > 1 ? (
+          <>
+            <span
+              className={classes.PreviousVideoButton}
+              onClick={props.decrementVideoIndex}
+            >
+              <ArrowBackIosRoundedIcon fontSize='large' />
+            </span>
 
-    return <div className={classes.Result}>
-        <h2>"{applyAccent(props.video.sentence, props.word, {color: 'var(--accent)', fontWeight: '700'})}"</h2>
-        <div className={classes.VideoBox}>
-            <div style={{borderRadius: '10px', overflow: 'hidden', height: '100%'}}>
-                <iframe 
-                title='main' 
-                width="100%" 
-                height="100%" 
-                src={`https://www.youtube-nocookie.com/embed/${props.video.id}?controls=0&start=${props.video.start}&end=${props.video.end}&autoplay=true`} 
-                frameborder="0" 
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                allowfullscreen></iframe>
-            </div>
-            
-            {/* Video Navigation Elements:  */}
-            {props.numVideos > 1 ?
-                <>
-                    <div className={classes.NodeBox}>
-                        {nodeList.map(node => {
-                            return <div 
-                            className={classes.Node} 
-                            style={node.active ? {opacity: '.9'} : null}
-                            onClick={node.clicked}></div>
-                        })}
-                    </div>
-                    <span className={classes.PreviousVideoButton} onClick={props.decrementVideoIndex}>
-                        <ArrowBackIosRoundedIcon fontSize='large'/>
-                    </span>
+            <span
+              className={classes.NextVideoButton}
+              onClick={props.incrementVideoIndex}
+            >
+              <ArrowForwardIosRoundedIcon fontSize='large' />
+            </span>
+          </>
+        ) : null}
+      </div>
 
-                    <span className={classes.NextVideoButton} onClick={props.incrementVideoIndex}>
-                        <ArrowForwardIosRoundedIcon fontSize='large'/>
-                    </span>
-                </>
-            : null}
-            
-        </div>
+      <h2>
+        "
+        {applyAccent(props.video.sentence, props.word, {
+          color: 'var(--accent)',
+          fontWeight: '700'
+        })}
+        "
+      </h2>
+
+      {renderPagination()}
     </div>
+  );
 }
 
 export default Result;
