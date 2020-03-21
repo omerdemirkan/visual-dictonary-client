81ddde01b0db055a9057713947be0102de1fde85
hamittokay <hamittokay.me@gmail.com>
2020-03-20 20:53:13 -0700

    Fix: Linting

--- src/utils/applyAccent.js
+++ src/utils/applyAccent.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 function applyAccent(text, highlight, style) {
-
-    let parts = text.split(new RegExp(`\w*(${highlight})\w*`, 'gi'));
-    return parts.map(part => part.toLowerCase() === highlight.toLowerCase() ? <span style={style}>{part}</span> : part);
+  let parts = text.split(new RegExp(`\w*(${highlight})\w*`, 'gi'));
+  return parts.map((part, idx) =>
+    part.toLowerCase() === highlight.toLowerCase() ? (
+      <span key={idx} style={style}>
+        {part}
+      </span>
+    ) : (
+      part
+    )
+  );
 }
 
 export default applyAccent;
