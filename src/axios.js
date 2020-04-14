284ec2f71ce5d4588a7c7681bbe9763b04fa6aca
omerdemirkan <omerfarukpiano@gmail.com>
2020-04-14 09:06:12 -0700

    update api url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://35.192.176.143:3000'
+    baseURL: 'https://api-424n3l7rpa-uw.a.run.app/'
+    // baseURL: 'https:localhost:5000'
 });
 
 export default instance;
